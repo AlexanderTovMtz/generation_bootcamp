11347430fbcca5112ccfea55b707bc8a16235423
¨AlexanderTovMtz¨ <alexander.tovarmtz@gmail.com>
2022-04-29 02:02:25 -0500

    29042022

---
+++ JavaScript/ciclos/assets/js/ciclos.js
@@ -0,0 +1,55 @@
+/*Ciclos*/
+
+//for
+//for ...in
+//.forEach
+
+//imprimir 
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<i; j++){
+//         document.write(`*`);
+//         // console.log(`\n`);
+
+//     }
+
+//     document.write(`<br>`);
+    
+// }
+
+// ejercicio1
+
+for (let a = 5; a>0; a--) {
+
+    a = a + 0.5;
+    console.log(a);
+    console.log(`\n`);    
+}
+
+// ejercicio2
+
+// for(let i=1; i<=100; i++) {
+//     if(i%2 > 1) {
+//         console.log(i);
+//     }
+// }
+
+// ejercicio3
+
+// const numero = prompt("ingresa un numero");
+// let control = 0;
+// let arreglo = [];
+// while(control < numero){
+//     arreglo[control] = control + 1;
+//     console.log(`[$(arreglo[control])]`);
+//     control ++;
+// }
+
+// ejercicio4
+
+// let numero = prompt("Ingresa un número:");
+// let suma = 0;
+// for(let i=1; i<=numero; i++) {
+//     suma = suma + i;
+// }
+// console.log("El resultado es: " + suma);
